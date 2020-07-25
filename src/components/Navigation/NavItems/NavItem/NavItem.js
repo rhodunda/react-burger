96a1b8662d38e052f678edce7be21f00993149f2
Bryan Rhodunda <rhodunda@udel.edu>
2020-07-25 00:19:08 -0400

    added orders tab

--- src/components/Navigation/NavItems/NavItem/NavItem.js
+++ src/components/Navigation/NavItems/NavItem/NavItem.js
@@ -1,13 +1,14 @@
 import React from 'react'
 // import navigationItems from '../NavItems'
 import classes from './NavItem.css'
+import { NavLink } from 'react-router-dom'
 
 
 const  navigationItem = (props) => (
     <li className={classes.NavigationItem}>
-        <a href={props.link} 
-        className={props.active ? classes.active : null}> 
-        {props.children} </a>
+        <Navlink to={props.link}> 
+        {props.children}
+        </Navlink>
     </li>
 )
 
