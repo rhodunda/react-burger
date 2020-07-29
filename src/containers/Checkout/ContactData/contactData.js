e0a2028d517ad9d76ac364b2420d6fb4d50f16b8
Bryan Rhodunda <rhodunda@udel.edu>
2020-07-29 15:18:36 -0400

    creating forms

--- src/containers/Checkout/ContactData/contactData.js
+++ src/containers/Checkout/ContactData/contactData.js
@@ -3,16 +3,27 @@ import Button from '../../../components/UI/Button/Button'
 import classes from './contactData.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spiner/Spinner'
+import Input from '../../../components/UI/Input/input'
 
 class ContactData extends Component {
 
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: '',
-        },
+       orderForm: {
+           name: {
+               elementType: 'input',
+               elementConfig: {
+                   type: 'text',
+                   placeholder: 'Your Name'
+               },
+               value: ''
+           },
+            name: 'Bryan Rhodunda',
+            street: '2619 Fogg lane',
+            zipCode: '19808',
+            country: 'United State of America',
+            email: 'rhodunda@udel.edu',
+            deliveryMethod: 'fastest'
+       },
         loading: false
     }
 
@@ -25,16 +36,7 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Bryan Rhodunda',
-                address: {
-                    street: '2619 Fogg lane',
-                    zipCode: '19808',
-                    country: 'United State of America'
-                },
-                email: 'rhodunda@udel.edu'
-            },
-            deliveryMethod: 'fastest'
+            
         }
         axios.post( '/orders.json', order )
             .then( response => {
@@ -50,10 +52,10 @@ class ContactData extends Component {
 
             let form = (
                 <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Name"/>
-                    <input className={classes.Input} type="email" name="email" placeholder="E-mail"/>
-                    <input className={classes.Input} type="text" name="street" placeholder="Street"/>
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code"/>
+                    <Input inputtype='input' type="text" name="name" placeholder="Name"/>
+                    <Input inputtype='input' type="email" name="email" placeholder="E-mail"/>
+                    <Input inputtype='input' type="text" name="street" placeholder="Street"/>
+                    <Input inputtype='input' type="text" name="postal" placeholder="Postal Code"/>
                     <Button  clicked={this.orderHandler} btnType="Success">ORDER</Button>
                 </form>
                 );
--- src/containers/Order/Orders.js
+++ src/containers/Order/Orders.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 import Order from '../Order/Order'
-
+import axios from '../../axios-orders'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 class Orders extends Component {
 
+state = {
+    orders: [],
+    loading: true
+}
+
+    componentDidMount() {
+        axios.get('/orders.json')
+        .then(resp => {
+            const fetchOrders = []
+            for (let key in resp.data) {
+                    fetchOrders.push({
+                        ...resp.data[key], 
+                    id: key})
+            }
+            this.setState({loading: false, orders: fetchOrders})
+        })
+        .catch(err => {
+            this.setState({loading: false})
+        })
+    }
 
     render() {
         return (
             <div>
-                <Order/>
-                <Order/>
+                {this.state.orders.map(order => (
+                    <Order key={order.id}
+                    ingredients={order.ingredients}
+                    price={+order.price}/>
+                ))}
             </div>
         )
     }
 
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios)
