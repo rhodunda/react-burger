dfe4b8d795299f235e36ebd3154addb652abaa58
Bryan Rhodunda <rhodunda@udel.edu>
2020-08-19 01:05:32 -0400

    added redux store

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -20,7 +20,7 @@ class BurgerBuilder extends Component {
     
     state = {
         ingredients: null,
-        totalPrice: 4,
+        totalPrice: 3,
         purchasable: false,
         purchasing: false,
         loading: false,
